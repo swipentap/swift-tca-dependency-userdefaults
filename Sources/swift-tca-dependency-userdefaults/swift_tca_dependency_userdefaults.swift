94d7a4309e2e21024bacf93be8efc0b5546390f0
Andrii Pylypenko <shadowchamber@icloud.com>
2023-12-23 02:54:01 +0200

    upd

--- Sources/swift-tca-dependency-userdefaults/swift_tca_dependency_userdefaults.swift
+++ Sources/swift-tca-dependency-userdefaults/swift_tca_dependency_userdefaults.swift
@@ -11,47 +11,35 @@ extension DependencyValues {
 
 extension Appl.Dependencies {
     public struct UserDefaultsClient {
-        
-        public enum Key: String {
-            case showUUIDInCatalogListItems
-            case boxBarcode
-            case startBarcodeNumber
-            case barcodeLabel
-            case printerAddress
-            case printerPort
-            case nopAPIURL
-            case calendar
-        }
-        
-        public var boolForKey: @Sendable (Key) -> Bool
-        public var dataForKey: @Sendable (Key) -> Data?
-        public var doubleForKey: @Sendable (Key) -> Double
-        public var integerForKey: @Sendable (Key) -> Int
-        public var stringForKey: @Sendable (Key) -> String
-        public var remove: @Sendable (Key) async -> Void
-        public var setBool: @Sendable (Bool, Key) async -> Void
-        public var setData: @Sendable (Data?, Key) async -> Void
-        public var setDouble: @Sendable (Double, Key) async -> Void
-        public var setInteger: @Sendable (Int, Key) async -> Void
-        public var setString: @Sendable (String, Key) async -> Void
+        public var boolForKey: @Sendable (String) -> Bool
+        public var dataForKey: @Sendable (String) -> Data?
+        public var doubleForKey: @Sendable (String) -> Double
+        public var integerForKey: @Sendable (String) -> Int
+        public var stringForKey: @Sendable (String) -> String
+        public var remove: @Sendable (String) async -> Void
+        public var setBool: @Sendable (Bool, String) async -> Void
+        public var setData: @Sendable (Data?, String) async -> Void
+        public var setDouble: @Sendable (Double, String) async -> Void
+        public var setInteger: @Sendable (Int, String) async -> Void
+        public var setString: @Sendable (String, String) async -> Void
     }
 }
 
 extension Appl.Dependencies.UserDefaultsClient: DependencyKey {
     public static let liveValue: Self = {
-        let defaults = { UserDefaults(suiteName: "group.keepstuff.live")! }
+        let defaults = { UserDefaults(suiteName: "group.app.live")! }
         return Self(
-            boolForKey: { defaults().bool(forKey: $0.rawValue) },
-            dataForKey: { defaults().data(forKey: $0.rawValue) },
-            doubleForKey: { defaults().double(forKey: $0.rawValue) },
-            integerForKey: { defaults().integer(forKey: $0.rawValue) },
-            stringForKey: { defaults().string(forKey: $0.rawValue) ?? "" },
-            remove: { defaults().removeObject(forKey: $0.rawValue) },
-            setBool: { defaults().set($0, forKey: $1.rawValue) },
-            setData: { defaults().set($0, forKey: $1.rawValue) },
-            setDouble: { defaults().set($0, forKey: $1.rawValue) },
-            setInteger: { defaults().set($0, forKey: $1.rawValue) },
-            setString: { defaults().set($0, forKey: $1.rawValue) }
+            boolForKey: { defaults().bool(forKey: $0) },
+            dataForKey: { defaults().data(forKey: $0) },
+            doubleForKey: { defaults().double(forKey: $0) },
+            integerForKey: { defaults().integer(forKey: $0) },
+            stringForKey: { defaults().string(forKey: $0) ?? "" },
+            remove: { defaults().removeObject(forKey: $0) },
+            setBool: { defaults().set($0, forKey: $1) },
+            setData: { defaults().set($0, forKey: $1) },
+            setDouble: { defaults().set($0, forKey: $1) },
+            setInteger: { defaults().set($0, forKey: $1) },
+            setString: { defaults().set($0, forKey: $1) }
         )
     }()
 }
--- Tests/swift-tca-dependency-userdefaultsTests/swift_tca_dependency_userdefaultsTests.swift
+++ Tests/swift-tca-dependency-userdefaultsTests/swift_tca_dependency_userdefaultsTests.swift
@@ -8,5 +8,15 @@ final class swift_tca_dependency_userdefaultsTests: XCTestCase {
 
         // Defining Test Cases and Test Methods
         // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+        XCTAssertEqual("test", "test")
+    }
+
+    func testExample2() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+        XCTAssertEqual("test", "test")
     }
 }
